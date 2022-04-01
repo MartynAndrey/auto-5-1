128f0cbe500e6e397a32c64f5ed51f7e52edb6b2
Andrey Martyn <martynandrey@rambler.ru>
2022-04-01 12:35:58 +0600

    Modified test for metting replan check

--- src/test/java/ru/netology/delivery/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryFormTest.java
@@ -12,40 +12,28 @@ public class CardDeliveryFormTest {
 
     @Test
     public void shouldSendOrderForm() {
-        RegistrationInfoManager rim = new RegistrationInfoManager();
-        rim.generateInfo();
+        RegistrationInfo startInfo = RegistrationInfoGenerator.generateRegistrationInfo();
+        String newDate = RegistrationInfoGenerator.generateDate();
 
         open("http://localhost:9999/");
         SelenideElement form = $("form");
-        form.$("[data-test-id='city'] input").setValue(rim.getInfo().getCity());
+        form.$("[data-test-id='city'] input").setValue(startInfo.getCity());
         form.$("[data-test-id='date'] input").doubleClick();
         form.$("[data-test-id='date'] input").sendKeys(Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(rim.getInfo().getDate());
-        form.$("[data-test-id='name'] input").setValue(rim.getInfo().getName());
-        form.$("[data-test-id='phone'] input").setValue(rim.getInfo().getPhone());
+        form.$("[data-test-id='date'] input").setValue(startInfo.getDate());
+        form.$("[data-test-id='name'] input").setValue(startInfo.getName());
+        form.$("[data-test-id='phone'] input").setValue(startInfo.getPhone());
         form.$("[data-test-id='agreement']").click();
         form.$(".button").click();
-        $("[data-test-id='success-notification']").shouldHave(Condition.text("Встреча успешно запланирована на " + rim.getInfo().getDate()));
+        $("[data-test-id='success-notification']").shouldHave(Condition.text("Встреча успешно запланирована на " + startInfo.getDate()));
         $("[data-test-id='success-notification'] .icon-button").click();
 
-        rim.changeDate();
         form.$("[data-test-id='date'] input").doubleClick();
         form.$("[data-test-id='date'] input").sendKeys(Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(rim.getInfo().getDate());
+        form.$("[data-test-id='date'] input").setValue(newDate);
         form.$(".button").click();
         $("[data-test-id='replan-notification']").shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $("[data-test-id='replan-notification'] .button").click();
-        $("[data-test-id='success-notification']").shouldHave(Condition.text("Встреча успешно запланирована на " + rim.getInfo().getDate()));
-    }
-
-    @Test
-    public void shouldGenerate() {
-        RegistrationInfoManager rim = new RegistrationInfoManager();
-        rim.generateInfo();
-        String city = rim.getInfo().getCity();
-        String date = rim.getInfo().getDate();
-        String name = rim.getInfo().getName();
-        String phone = rim.getInfo().getPhone();
-        System.out.println("Done");
+        $("[data-test-id='success-notification']").shouldHave(Condition.text("Встреча успешно запланирована на " + newDate));
     }
 }
