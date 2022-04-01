8c5a6bffa470b93936a905951620e4ccf9e6180e
Andrey Martyn <martynandrey@rambler.ru>
2022-04-01 12:25:56 +0600

    Modified RegistrationInfo class (use Lombok)

--- src/test/java/ru/netology/delivery/RegistrationInfo.java
+++ src/test/java/ru/netology/delivery/RegistrationInfo.java
@@ -1,40 +1,11 @@
 package ru.netology.delivery;
 
+import lombok.Data;
+
+@Data
 public class RegistrationInfo {
     private String city;
     private String date;
     private String name;
     private String phone;
-
-    public String getCity() {
-        return this.city;
-    }
-
-    public String getDate() {
-        return this.date;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getPhone() {
-        return this.phone;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
